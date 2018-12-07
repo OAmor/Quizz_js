9e50e98e88d97dd64a99788fbe024e61562b1229
Oussama <ahmedoussama.amor@etu.usthb.dz>
2018-12-07 20:39:21 +0100

    add data structure

---
+++ data/data.js
@@ -0,0 +1,106 @@
+
+var data = {
+    'cours1':[
+        {question:'test question 1',
+            response:{
+            variable: 'a',
+            content : 'test response 1'
+         }
+        },
+        {question:'test question 2',
+            response:{
+                variable: 'b',
+                content : 'test response 1'
+            }
+        },
+        {question:'test question 3',
+            response:{
+                variable: 'c',
+                content : 'test response 1'
+            }
+        },
+        {question:'test question 0',
+            response:{
+                variable: 'd',
+                content : 'test response 1'
+            }
+        }
+    ],
+    'cours2':[
+        {question:'test question 1',
+            response:{
+                variable: 'a',
+                content : 'test response 1'
+            }
+        },
+        {question:'test question 2',
+            response:{
+                variable: 'b',
+                content : 'test response 1'
+            }
+        },
+        {question:'test question 3',
+            response:{
+                variable: 'c',
+                content : 'test response 1'
+            }
+        },
+        {question:'test question 0',
+            response:{
+                variable: 'd',
+                content : 'test response 1'
+            }
+        }
+    ],'cours3':[
+        {question:'test question 1',
+            response:{
+                variable: 'a',
+                content : 'test response 1'
+            }
+        },
+        {question:'test question 2',
+            response:{
+                variable: 'b',
+                content : 'test response 1'
+            }
+        },
+        {question:'test question 3',
+            response:{
+                variable: 'c',
+                content : 'test response 1'
+            }
+        },
+        {question:'test question 0',
+            response:{
+                variable: 'd',
+                content : 'test response 1'
+            }
+        }
+    ],'cours4':[
+        {question:'test question 1',
+            response:{
+                variable: 'a',
+                content : 'test response 1'
+            }
+        },
+        {question:'test question 2',
+            response:{
+                variable: 'b',
+                content : 'test response 1'
+            }
+        },
+        {question:'test question 3',
+            response:{
+                variable: 'c',
+                content : 'test response 1'
+            }
+        },
+        {question:'test question 0',
+            response:{
+                variable: 'd',
+                content : 'test response 1'
+            }
+        }
+    ]
+}
+
