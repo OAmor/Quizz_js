b1027730c876da5355f1d83699f05aa3a0b677ff
Oussama <ahmedoussama.amor@etu.usthb.dz>
2018-12-07 21:29:11 +0100

    add show results feature

--- scripts/main.js
+++ scripts/main.js
@@ -9,12 +9,12 @@ function getResponse(question) {
     }
 }
 
-
-
 function calcScore(course) {
     var nbr = document.getElementById('nbr_quest').value,
         score = 0,
-        quizz;
+        quizz,
+        questions = document.getElementsByClassName('question-content'),
+        result = document.getElementById('result');
 
     switch (course){
         case 1 : quizz = data.cours1;break;
@@ -23,15 +23,26 @@ function calcScore(course) {
         case 4 : quizz = data.cours4;break;
     }
 
-    console.log(nbr);
-
     for(var i=1; i<=nbr;i++){
-        var res = getResponse('q'+i);
+        var j,res = getResponse('q'+i),
+            answer = quizz[i-1].response.variable,
+            answers = questions[i-1].getElementsByTagName('p');
+
         console.log("compare q"+i +"= "+res+" with : "+quizz[i-1].response.variable);
-        if(res == quizz[i-1].response.variable){
+        if(res == answer){
             score++;
+        }
+
+        for(j = 0;j<answers.length;j++){
+            if(answers[j].getElementsByTagName('input')[0].value == answer){
+                answers[j].style.color = "#080"
+            }else if(answers[j].getElementsByTagName('input')[0].value == res){
+                answers[j].style.color = "#f00"
+            }
         }
     }
 
-    alert(score)
+
+    result.innerText = "Votre Score = "+score + " / "+nbr;
+    result.style.display = "block"
 }
