3e3237eda38a3a3aebb6ed1abc85880c4bc5e1a1
KERFAH Issam <ikerfah@gmail.com>
2018-12-14 02:33:20 +0100

    Format document

--- data/data.js
+++ data/data.js
@@ -1,31 +1,31 @@
 
 var data = {
-    'java':[
-        {variable: 'b'},
-        {variable: 'b'},
-        {variable: 'c'},
-        {variable: 'd'}
+    'java': [
+        { variable: 'b' },
+        { variable: 'b' },
+        { variable: 'c' },
+        { variable: 'd' }
     ],
-    'android':[
-        {variable: 'a'},
-        {variable: 'd'},
-        {variable: 'a'},
-        {variable: 'c'}
-    ],'kotlin':[
-        {variable: 'a'},
-        {variable: 'b'},
-        {variable: 'a'},
-        {variable: 'c'}
-    ],'git':[
-        {variable: 'c'},
-        {variable: 'a'},
-        {variable: 'a'},
-        {variable: 'b'}
-    ],'cpp':[
-        {variable: 'a' },
-        {variable: 'c'},
-        {variable: 'b'},
-        {variable: 'c'}
+    'android': [
+        { variable: 'a' },
+        { variable: 'd' },
+        { variable: 'a' },
+        { variable: 'c' }
+    ], 'kotlin': [
+        { variable: 'a' },
+        { variable: 'b' },
+        { variable: 'a' },
+        { variable: 'c' }
+    ], 'git': [
+        { variable: 'c' },
+        { variable: 'a' },
+        { variable: 'a' },
+        { variable: 'b' }
+    ], 'cpp': [
+        { variable: 'a' },
+        { variable: 'c' },
+        { variable: 'b' },
+        { variable: 'c' }
     ]
 }
 
